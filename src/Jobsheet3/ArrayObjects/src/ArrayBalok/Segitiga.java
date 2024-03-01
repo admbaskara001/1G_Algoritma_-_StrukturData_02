1c106ffef8091ce491c261ccf19044c8a4d1252a
admbaskara001 <adhambaskara2@gmail.com>
2024-03-02 00:24:36 +0700

    P3_Percobaan3_Pertanyaan2,3,4,5

---
+++ src/Jobsheet3/ArrayObjects/src/ArrayBalok/Segitiga.java
@@ -0,0 +1,20 @@
+package Jobsheet3.ArrayObjects.src.ArrayBalok;
+
+public class Segitiga {
+        public int alas;
+        public int tinggi;
+    
+        public Segitiga(int a, int t) {
+            alas = a;
+            tinggi = t;
+        }
+    
+        public double hitungLuas() {
+            return 0.5 * alas * tinggi;
+        }
+    
+        public double hitungKeliling() {
+            double sisiMiring = Math.sqrt(alas * alas + tinggi * tinggi);
+            return alas + tinggi + sisiMiring;
+        }
+    }
