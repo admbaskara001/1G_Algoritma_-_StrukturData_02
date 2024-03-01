e38a12cdf7748486b1e855141b75aa36e4b7dd9f
admbaskara001 <adhambaskara2@gmail.com>
2024-03-01 22:10:20 +0700

    P3_Percobaan2_Pertanyaan4

--- src/Jobsheet3/ArrayObjects/src/Main.java
+++ src/Jobsheet3/ArrayObjects/src/Main.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];
-        Scanner sc02 = new Scanner(System.in);
-
-        for (int i = 0; i < 3; i++) {
-            
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan panjang array: ");
+        int panjangArray = scanner.nextInt();
+        
+        PersegiPanjang[] ppArray = new PersegiPanjang[panjangArray];
+        
+        for (int i = 0; i < panjangArray; i++) {
             ppArray[i] = new PersegiPanjang();
-            System.out.print("Persegi panjang ke-" + i);
-            System.out.println("Masukkan Panjang: ");
-            ppArray[i].panjang = sc02.nextInt();
-            System.out.println("Masukkan Lebar: ");
-            ppArray[i].lebar = sc02.nextInt();
+            System.out.print("Persegi panjang ke-" + i + "\nMasukkan Panjang: ");
+            ppArray[i].panjang = scanner.nextInt();
+            System.out.print("Masukkan Lebar: ");
+            ppArray[i].lebar = scanner.nextInt();
         }
-        for (int i = 0; i < 3; i++) {
+        
+        for (int i = 0; i < panjangArray; i++) {
             System.out.println("Persegi panjang ke-" + i); 
             System.out.println("Panjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
         }
+        
+        scanner.close();
     }
 }
