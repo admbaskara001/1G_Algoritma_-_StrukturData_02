550c36937a673a1e675e33c0ac573f6c33177068
admbaskara001 <adhambaskara2@gmail.com>
2024-03-02 10:22:37 +0700

    P3_Praktikum3_02

--- src/Jobsheet3/ArrayObjects/src/Praktikum2/Main.java
+++ src/Jobsheet3/ArrayObjects/src/Praktikum2/Main.java
@@ -21,9 +21,22 @@ public class Main {
             mahasiswa[i] = new Mahasiswa(nama, nim, jenisKelamin, ipk);
         }
 
+        double totalIpk = 0;
+        double maxIpk = 0;
+        Mahasiswa mahasiswaDenganIpkTertinggi = null;
         for (int i = 0; i < 3; i++) {
             System.out.println("\nData Mahasiswa ke-" + (i + 1));
             mahasiswa[i].tampilkanInfo();
+            totalIpk += mahasiswa[i].ipk;
+            if (mahasiswa[i].ipk > maxIpk) {
+                maxIpk = mahasiswa[i].ipk;
+                mahasiswaDenganIpkTertinggi = mahasiswa[i];
+            }
         }
+
+        double rataRataIpk = totalIpk / 3;
+        System.out.println("\nRata-rata IPK: " + rataRataIpk);
+        System.out.println("\nMahasiswa dengan IPK tertinggi:");
+        mahasiswaDenganIpkTertinggi.tampilkanInfo();
     }
 }
